ecdf4e72cc129dd6003b3421ac9f52ab8f0125a0
s20071037 <s20071037@gmail.com>
2026-02-24 22:02:36 +0800

    Update Day1_TwoSum.java
    Add hints and problem from leetcode

--- Day1_TwoSum.java
+++ Day1_TwoSum.java
@@ -1,5 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+*/
+
 public class Day1_TwoSum {
     public int[] twoSum(int[] nums, int target) {
+
+/*
+Hint 3
+The second train of thought is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?
+*/
+
+// can assume always has 1 solution
         Map<Integer, Integer> map = new HashMap<>();
         for(int i =0; i < nums.length; i++){
             if(map.containsKey(target-nums[i])){
@@ -9,6 +42,5 @@ public class Day1_TwoSum {
         }
         return null;
 
-        
     }
 }
